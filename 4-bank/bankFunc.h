c88d19e033906e052e10432b8bbb4855da63428f
Marley Eduardo <116856059+Marleyedrg@users.noreply.github.com>
2024-07-20 14:59:34 -0300

    almost finished

--- 4-bank/bankFunc.h
+++ 4-bank/bankFunc.h
@@ -15,13 +15,16 @@
 #define WHT "\e[0;37m"
 #define RED "\e[0;31m"
 
+#define MAX_LINE_LENGTH 100
 
 void startBank(User *user, int option);
-void createUserAcc(char *userName,char *userAcc);
+char* getUserAcc(char *userName,char *userAcc);
+char* extractAttributeValue(char *buffer,char *attribute);
 void deposit(User *user);
-void withdraw();
+void withdraw(User *user);
 void transfer();
-void accDetails();
+void accDetails(User *user);
+void addTraDetails(char*time,char *userName,char *userAcc, char *type, double value);
 void traDetails();
 
 
@@ -30,19 +33,18 @@ void startBank(User *user, int option)
     time_t tm;
     time(&tm);
     printf("%s", ctime(&tm));
-
     switch (option) {
         case 1:
             deposit(user);
             break;
         case 2:
-            withdraw();
+            withdraw(user);
             break;
         case 3:
             transfer();
             break;
         case 4:
-            accDetails();
+            accDetails(user);
             break;
         case 5:
             traDetails();
@@ -52,49 +54,178 @@ void startBank(User *user, int option)
         }
 }
 
-void createUserAcc(char *userName,char *userAcc){
+char* getUserAcc(char *userName,char *userAcc){
+
+    time_t tm;
+    time(&tm);
+
     char *buffer;
 
     buffer = (char *) malloc(59);
 
-    printf("\n%s",userName);
-
-    sprintf(buffer, "accounts/%s",userName);
+    sprintf(buffer, "accounts/%s%s",userName,userAcc);
 
     FILE *crAcc = fopen(buffer, "a");
 
-    fprintf(crAcc, "name: %s\naccount number: %s",userName,userAcc);
+    fseek(crAcc, 0, SEEK_END);
 
-    char *line = (char *) malloc(250);
+    long fileSize = ftell(crAcc);
 
-    
-
-    free(buffer);
-    free(userName);
-    free(userAcc);
-
+    if (fileSize == 0)
+    {
+        fprintf(crAcc, "account created in: %sname: %s\naccount number: %s\nbalance: R$0.00",ctime(&tm),userName,userAcc);
+    }
     fclose(crAcc);
+    return buffer;
 }
 
+char* extractAttributeValue(char *buffer, char *attribute){
+    char *value;
+    value = (char *) malloc(MAX_LINE_LENGTH-10);
+    char line[MAX_LINE_LENGTH];
+    char *token;
+
+    FILE *file = fopen(buffer, "r");
+    FILE *tempFile = fopen("accounts/temp", "w");
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        
+        if (strstr(line, attribute) == line) {
+            
+            token = strtok(line, "$");  
+            if (token != NULL) {
+                token = strtok(NULL, "$"); 
+
+                if (token != NULL) {
+                    strcpy(value, token);
+                    break;
+                }
+            }
+        }
+        fputs(line, tempFile);
+    }
+    fclose(file);
+    fclose(tempFile);
+
+    remove(buffer);
+
+    rename("accounts/temp", buffer);
+
+    return value;
+}
 inline void deposit(User *user)
 {
-    createUserAcc(user->name,user->accNo);
+    time_t tm;
+    time(&tm);
+
+    double deposit;
+    char *strAccValue;
+    double oldValue;
+    strAccValue = (char *) malloc(MAX_LINE_LENGTH-10);
+
+    char *cpBuffer = getUserAcc(user->name,user->accNo);
+
+    printf("\nAmount to deposit: ");
+    (scanf("%le",&deposit));
+
+    strAccValue = extractAttributeValue(cpBuffer, "balance:");
+
+    oldValue = strtod(strAccValue, NULL);
+
+    FILE *crAcc = fopen(cpBuffer, "a");
+
+    fprintf(crAcc,"balance: R$%.2f\n",(oldValue+deposit));
+
+    addTraDetails(ctime(&tm),user->name,user->accNo,"DEPOSIT",deposit);
 }
 
-inline void withdraw()
-{
+inline void withdraw(User *user){
+    time_t tm;
+    time(&tm);
+
+    double withdraw;
+    char *strAccValue;
+    double oldValue;
+    strAccValue = (char *) malloc(MAX_LINE_LENGTH-10);
+
+    char *cpBuffer = getUserAcc(user->name,user->accNo);
+
+    printf("\nAmount to withdraw: ");
+    (scanf("%le",&withdraw));
+
+    strAccValue = extractAttributeValue(cpBuffer, "balance:");
+
+    oldValue = strtod(strAccValue, NULL);
+
+    FILE *crAcc = fopen(cpBuffer, "a");
+
+    if ((oldValue - withdraw) < 0)
+    {
+        printf(RED"\ninsufficient funds!\n"WHT);
+        fprintf(crAcc,"balance: R$%.2f\n",(oldValue));
+    }else{
+    fprintf(crAcc,"balance: R$%.2f\n",(oldValue-withdraw));
+    }
+
+    addTraDetails(ctime(&tm),user->name,user->accNo,"WITHDRAW",withdraw);
 }
 
 inline void transfer()
 {
 }
 
-inline void accDetails()
+inline void accDetails(User *user)
 {
+    char *cpBuffer = getUserAcc(user->name,user->accNo);
+
+    FILE *file;
+    char line[MAX_LINE_LENGTH];
+
+    file = fopen(cpBuffer, "r");
+    
+    printf("%s\n", "-----------------");
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+
+    printf("%s", "-----------------");
+
+    fclose(file);
 }
 
 inline void traDetails()
 {
+    FILE *file;
+    char line[MAX_LINE_LENGTH];
+
+
+    file = fopen("traDetails", "r");
+
+    printf("\n");
+
+    while (fgets(line, sizeof(line), file) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(file);
 }
+
+void addTraDetails(char*time,char *userName,char *userAcc, char *type, double value){
+    FILE *file;
+
+    file = fopen("traDetails", "a");
+
+    fprintf(file, "date of transaction: %s%s\nname: %s\naccount number: %s\nvalue: %.2f\n",time,type,userName,userAcc,value);
+    fprintf(file,"----\n");
+}
+// void addTranferDetails(char*time,char *userName,char *userAcc, char *type, double value){
+//     FILE *file;
+
+//     file = fopen("traDetails", "a");
+
+//     fprintf(crAcc, "date of transaction: %sname: %s\naccount number: %s\n%s\nvalue: %ld\n",timeTra,userName,userAcc,type,value);
+//     printf("%s\n", "-----------------");
+// }
 
 
